c49373c5434ab61529d5e5c0f2272238f09f83ed
Pritam <pritamdebnath297@gmail.com>
2021-05-06 17:00:27 +0530

    feat: added login feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+  if(!localStorage.getItem('username')) return <LoginForm/>
   return(
     <ChatEngine
       height="100vh"
       projectID="443eaa57-95f6-4e88-8d93-afdcc65c9792"
-      userName="Pritam"
-      userSecret="1234567890"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
     />
 
